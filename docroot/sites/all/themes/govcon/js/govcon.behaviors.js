3b17fe4d7c47c845df81dddf20d8f8d4e66fe013
jonraedeke <jraedeke@gpo.gov>
2015-03-15 21:54:10 -0400

    styling for event date and location on homepage and conference name change callout

--- docroot/sites/all/themes/govcon/js/govcon.behaviors.js
+++ docroot/sites/all/themes/govcon/js/govcon.behaviors.js
@@ -10,10 +10,6 @@
    * In most cases, there is no good reason to NOT wrap your markup producing
    * JavaScript in a theme function.
    */
-  Drupal.theme.prototype.govconExampleButton = function (path, title) {
-    // Create an anchor element with jQuery.
-    return $('<a href="' + path + '" title="' + title + '">' + title + '</a>');
-  };
 
   Drupal.theme.prototype.govconBevelBox = function (e) {
     // Create bevel markup
@@ -49,24 +45,6 @@
    *   Drupal.settings directly you should use this because of potential
    *   modifications made by the Ajax callback that also produced 'context'.
    */
-  Drupal.behaviors.govconExampleBehavior = {
-    attach: function (context, settings) {
-      // By using the 'context' variable we make sure that our code only runs on
-      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
-      // we don't run the same piece of code for an HTML snippet that we already
-      // processed previously. By using .once('foo') all processed elements will
-      // get tagged with a 'foo-processed' class, causing all future invocations
-      // of this behavior to ignore them.
-      $('.some-selector', context).once('foo', function () {
-        // Now, we are invoking the previously declared theme function using two
-        // settings as arguments.
-        var $anchor = Drupal.theme('govconExampleButton', settings.myExampleLinkPath, settings.myExampleLinkTitle);
-
-        // The anchor is then appended to the current element.
-        $anchor.appendTo(this);
-      });
-    }
-  };
 
   Drupal.behaviors.govconBevelBox = {
     attach: function (context, settings) {
